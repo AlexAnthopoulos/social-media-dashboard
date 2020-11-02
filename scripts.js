e26853049526a7955cb987894eaf13e4ba46a7df
AlexAnthopoulos <alex.anthopoulos01@gmail.com>
2020-11-02 09:46:24 +0100

    more styling for dark mode and JS for the swap between light/dark

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const toggleButton = document.querySelector('#checkbox');
+const html = document.querySelector('html');
+
+function switchTheme(e) {
+  if (toggleButton.checked) {
+    html.className = 'theme-light';
+  } else {
+    html.className = 'theme-dark';
+  }
+}
+
+toggleButton.addEventListener('change', switchTheme);
+window.addEventListener('load', switchTheme);
